9f11818c0ca4e87b75af279c59329134d126b1b8
nikonyrh <niko.nyrhila@gmail.com>
2017-02-20 22:15:29 +0200

    Added joining with weather data

--- taxi-rides-clj/src/taxi_rides_clj/core.clj
+++ taxi-rides-clj/src/taxi_rides_clj/core.clj
@@ -26,7 +26,7 @@
      :double    double-parser
      :latlon   #(if-not (= % "0") (double-parser %))
      :keyword  #(let [s (string/trim %)] (if-not (empty? s) s))
-     :date     #(let[d (.parse date-format %)](str (.format ymd-format d) "T" (.format hms-format d) "Z"))}))
+     :date     #(let [d (.parse date-format %)] (str (.format ymd-format d) "T" (.format hms-format d) "Z"))}))
 
 (def col-mapping
   {"VendorID"                [:int     :vendor-id]
@@ -50,25 +50,43 @@
    "Trip_type"               [:keyword :trip-type]
    "Extra"                   nil})
 
+(let [[header & rows] (-> (str data-folder "/central_park_weather.csv") slurp csv/read-csv)
+      cols-to-keep #{"DATE" "PRCP" "SNWD" "SNOW" "TMAX" "TMIN" "AWND"}
+      n-cols (count header)
+      rows (for [row rows :when (>= (count row) n-cols)]
+             (into {} (map (fn [k v] (if (cols-to-keep k) [k v])) header row)))]
+  (def weather
+    (->> rows
+         (map (fn [row] (let [[y1 y2 m d] (partition 2 (row "DATE"))
+                              date (apply str (concat y1 y2 "-" m "-" d))]
+                          [date
+                           (->>
+                             (-> row (dissoc "DATE") seq)
+                             (map (fn [[k v]] [(keyword (str "weather-" k)) ((:double parsers) v)]))
+                             (into {}))])))
+         (into {}))))
+;(->> weather seq (take 10))
+
 (defn parse-trips [file]
   (with-open [in (-> file clojure.java.io/input-stream java.util.zip.GZIPInputStream.)]
-    (let [[header & rows] (-> in slurp csv/read-csv)
+    (let [doc-id-fields [:pickup-dt :dropff-dt :pickup-lat :pickup-lon :dropoff-lat :dropoff-lon :distance  :paid-total]
+          dataset (->> file str (re-find #"([a-z]+)[^/]+$") second)
+          [header & rows] (-> in slurp csv/read-csv)
           header   (map string/trim header)
           missing  (clojure.set/difference (-> header set) (-> col-mapping keys set))
           check    (assert (empty? missing) (str "Missing cols for " missing))
           header   (map col-mapping header)
-          n_cols   (count header)
-          doc-id-fields [:pickup-dt :dropff-dt :distance :pickup-lat :pickup-lon :dropoff-lat :dropoff-lon :paid-total]
-          docs     (for [row rows :when (>= (count row) n_cols)]
+          n-cols   (count header)
+          docs     (for [row rows :when (>= (count row) n-cols)]
                      (let [doc (into {} (map (fn [h v] (if h [(second h) ((-> h first parsers) v)])) header row))]
-                       (assoc doc :id (->> (interleave (map doc doc-id-fields) (repeat "/")) (apply str) sha1-hash))))]
+                       (-> doc
+                         (assoc :id      (->> doc-id-fields (map doc) (interleave (repeat "/")) (apply str) sha1-hash))
+                         (assoc :index   (apply str "nyc-taxi-" dataset "-" (take 7 (:pickup-dt doc))))
+                         (into           (weather (-> doc :pickup-dt (subs 0 10)))))))]
         (filter #(pos? (:distance %)) docs))))
 
-
-(def rows (->> files first parse-trips))
-(pprint (take 3 rows))
-(def freqs (into {} (for [key (keys (first rows))] [key (frequencies (map key rows))])))
-(pprint (into {} (for [[k v] freqs] [k (count v)])))
+(def rows (->> files first parse-trips (group-by :index)))
+(pprint (take 3 (rows (-> rows keys first))))
 
 (defn -main []
   (println "Hello, World!"))
