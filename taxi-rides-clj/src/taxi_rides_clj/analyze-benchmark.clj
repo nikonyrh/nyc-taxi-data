62cf99a1615d8caccc9681c77315fb4681299529
nikonyrh <niko.nyrhila@gmail.com>
2017-04-16 22:59:56 +0300

    Added Elasticsearch benchmark results from Intel 6700K system

--- taxi-rides-clj/src/taxi_rides_clj/analyze-benchmark.clj
+++ taxi-rides-clj/src/taxi_rides_clj/analyze-benchmark.clj
@@ -1,29 +1,30 @@
 (ns taxi-rides-clj.analyze-benchmark
   (:require [clojure.edn :as edn]
             [nikonyrh-utilities-clj.core :as u]
-            [com.hypirion.clj-xchart :as c])
-  (:import java.awt.image.BufferedImage))
+            [com.hypirion.clj-xchart :as c]))
 
 (set! *warn-on-reflection* true)
+(defn kw->str [kw] (-> kw str (subs 1)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def group-keys [:db :filter :aggs])
-(def results (->> ["results_es.edn"] (mapcat #(-> % slurp read-string))))
-(def modes   (into {} (for [type group-keys] [type (->> results (map type) set sort vec)])))
-
-(defn kw->str [kw] (-> kw str (subs 1)))
+(def results (->> ["es_6700k.edn"]
+                  (mapcat #(->> % (str "results/") slurp read-string))))
+(def modes (into {} (for [key group-keys] [key (->> results (map key) set sort vec)])))
 
 (let [group (apply juxt group-keys)]
   (def grouped-results (into {} (for [result results]
                                   [(group result)
                                    (->> result :stats (into (sorted-map)))]))))
 
+; https://hypirion.github.io/clj-xchart/examples
 (defn plot [res filter-kw aggs-kw]
   (->> (for [db (:db modes)]
          (let [data   (grouped-results [db filter-kw aggs-kw])
                d-keys (for [k (keys data) :when (-> k data pos?)] k)]
-           [db [d-keys
-                (map data d-keys)]]))
+           [db {:x    (map data d-keys)
+                :y    d-keys
+                :style {:marker-type :none}}]))
        (into {})
        (#(c/xy-chart % {:width  res
                         :height res
