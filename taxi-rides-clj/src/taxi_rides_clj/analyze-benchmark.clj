415a9e8a8f03f333d14cc5ee9ff044bd554610e8
nikonyrh <niko.nyrhila@gmail.com>
2017-04-24 11:01:36 +0300

    Added SQL results to plotting

--- taxi-rides-clj/src/taxi_rides_clj/analyze-benchmark.clj
+++ taxi-rides-clj/src/taxi_rides_clj/analyze-benchmark.clj
@@ -8,7 +8,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def group-keys [:db :filter :aggs])
-(def results (->> ["es_6700k.edn"]
+(def results (->> ["sql_4770.edn" "es_6700k_16g.edn" "es_6700k_32g.edn" "es_6700k_64g.edn"]
                   (mapcat #(->> % (str "results/") slurp read-string))))
 (def modes (into {} (for [key group-keys] [key (->> results (map key) set sort vec)])))
 
@@ -22,9 +22,11 @@
   (->> (for [db (:db modes)]
          (let [data   (grouped-results [db filter-kw aggs-kw])
                d-keys (for [k (keys data) :when (-> k data pos?)] k)]
-           [db {:x    (map data d-keys)
-                :y    d-keys
-                :style {:marker-type :none}}]))
+           (if (empty? d-keys)
+             (u/my-println "Empty for [\"" db "\"" filter-kw " " aggs-kw "]")
+             [db {:x    (map data d-keys)
+                  :y    d-keys
+                  :style {:marker-type :none}}])))
        (into {})
        (#(c/xy-chart % {:width  res
                         :height res
@@ -34,11 +36,8 @@
                         :legend {:visible?     false}}))))
 
 (defn plot-all [res]
-  (let [n-filters  (-> modes :filter count)
-        n-aggs     (-> modes :aggs   count)]
-    (->> (for [i-filter (range n-filters) i-aggs (range n-aggs)]
-           (plot res (-> modes :filter (nth i-filter))
-                     (-> modes :aggs   (nth i-aggs))))
-         (apply c/view))))
+  (->> (for [filter (modes :filter) agg (modes :aggs)]
+         (plot res filter agg))
+       (apply c/view)))
 
 ; (plot-all 400)
